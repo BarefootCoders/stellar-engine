21dd6a81f9f64a7fd4a55da8ad68bc9686ffd3ef
Saisiri Boggavarapu <siri.boggavarapu@darkwolfsolutions.com>
2025-04-29 19:02:24 +0000

    dr 612 app engine blueprint

--- blueprints/fedramp-high/app-engine/variables.tf
+++ blueprints/fedramp-high/app-engine/variables.tf
@@ -1,9 +1,10 @@
 variable "main_project_id" {
-  description = "Main Project ID to host App Engine. App Engine cannot be delete from the project once provisioned."
+  description = "Main project ID."
   type        = string
 }
 
 variable "region" {
-  description = "Region to create your App Engine resource."
+  description = "Google Cloud Region."
   type        = string
+  default     = "us-east4"
 }
